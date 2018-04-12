2f671e500e42e030f2cf57fd18b33fa0807ce7d4
Steven <steven.james.correia@gmail.com>
2018-04-12 14:33:30 +0200

    Adds list of other cats, functionality to load a new cat and play the game

--- js/app.js
+++ js/app.js
@@ -1,3 +1,53 @@
+var catData = [
+    {
+    clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    imgAttribution: 'https://www.flicker.com/photos/bigtallguy/434164568',
+    nicknames: [
+        { name: 'Mr. T' },
+        { name: 'The Terminator' },
+        { name: 'Tabbilicious' }
+        ]
+    },
+    {
+    clickCount: 0,
+    name: 'Moony',
+        imgSrc: 'img/22252709_010df3379e_z.jpg',
+    imgAttribution: 'https://www.flicker.com/photos/bigtallguy/434164568',
+    nicknames: [
+        { name: 'Moon Moon' }
+        ]
+    },
+    {
+    clickCount: 0,
+    name: 'Tiger',
+        imgSrc: 'img/1413379559_412a540d29_z.jpg',
+    imgAttribution: 'https://www.flicker.com/photos/bigtallguy/434164568',
+    nicknames: [
+        { name: 'Robot Jones' }
+        ]
+    },
+    {
+    clickCount: 0,
+    name: 'Bart',
+        imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+    imgAttribution: 'https://www.flicker.com/photos/bigtallguy/434164568',
+    nicknames: [
+        { name: 'Bart the Fart' }
+        ]
+    },
+    {
+    clickCount: 0,
+    name: 'Ziggy',
+        imgSrc: 'img/9648464288_2516b35537_z.jpg',
+    imgAttribution: 'https://www.flicker.com/photos/bigtallguy/434164568',
+    nicknames: [
+        { name: 'Starman' }
+        ]
+    },
+];
+
 var Cat = function(data) {
     this.clickCount = ko.observable(data.clickCount);
     this.name = ko.observable(data.name);
@@ -25,21 +75,22 @@ var Cat = function(data) {
 }
 
 var ViewModel = function () {
-    this.currentCat = ko.observable(new Cat({
-        clickCount: 0,
-        name: 'Tabby',
-        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-        imgAttribution: 'https://www.flicker.com/photos/bigtallguy/434164568',
-        nicknames: [
-            { name: 'Mr. T' },
-            { name: 'The Terminator' },
-            { name: 'Tabbilicious' }
-        ]
-    }));
+    var self = this;
+
+    this.catList = ko.observableArray([]);
+    catData.forEach(function(catItem){
+        self.catList.push(new Cat(catItem));
+    });
+
+    this.currentCat = ko.observable(this.catList()[0]);
 
     this.incrementCounter = function () {
         this.clickCount(this.clickCount() + 1);
     };
+
+    this.fetchCat = function (chosenCat) {
+        self.currentCat(chosenCat);
+    }
 
 }
 
