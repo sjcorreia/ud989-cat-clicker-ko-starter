54011add92d93999dd347be94ffe3e148a5b8d7b
Steven <steven.james.correia@gmail.com>
2018-04-12 12:31:04 +0200

    Separates the Model out from the ViewModel

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var ViewModel = function () {
+var Cat = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
@@ -8,10 +8,6 @@ var ViewModel = function () {
         { name: 'The Terminator' },
         { name: 'Tabbilicious' }
     ]);
-
-    this.incrementCounter = function () {
-        this.clickCount(this.clickCount() + 1);
-    };
 
     this.level = ko.computed(function () {
         if (this.clickCount() < 10) {
@@ -30,6 +26,14 @@ var ViewModel = function () {
             return 'Adult';
         }
     }, this);
+}
+
+var ViewModel = function () {
+    this.currentCat = ko.observable(new Cat());
+
+    this.incrementCounter = function () {
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    };
 
 }
 
